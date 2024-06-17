ea15e6a5a820fc4072c2ee8e1f332d4e47628657
Yernar Mussin <yernar.mussin@bgaming.com>
2024-06-17 22:19:14 +0500

    auth in progress

--- src/pages/SignIn.vue
+++ src/pages/SignIn.vue
@@ -1,40 +1,27 @@
 <script setup>
 import { ref } from 'vue'
 
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
+const email = ref('superadmin@example.com');
+const password = ref('secret');
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>Sign In</code> to test HMR
-    </p>
+  <div class="auth">
+    <div class="auth-content">
+      <h1 class="auth-content__title">Sign In</h1>
+      <h2 class="auth-content__sub-title">AI Chat | Авторизация</h2>
+    </div>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
+<style lang="sass" scoped>
+.auth
+  display: flex
+  justify-content: center
+  align-items: center
+
+  &-content
+    max-width: 1200px
+    width: 100%
+
 </style>
